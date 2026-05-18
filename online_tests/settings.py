03a0cbb2c2d396116476e9db9adc77a79a2db454
odil-star <ahmedovodil2002@gmail.com>
2026-05-18 16:01:01 +0500

    deploy beckend

--- online_tests/settings.py
+++ online_tests/settings.py
@@ -8,10 +8,7 @@ SECRET_KEY = "django-insecure-change-this-key-for-production"
 
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    "127.0.0.1",
-    "localhost",
-]
+ALLOWED_HOSTS = ["*"]
 
 INSTALLED_APPS = [
     "django.contrib.admin",
@@ -75,6 +72,7 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [BASE_DIR / "static"]
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
